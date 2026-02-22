2f05f09b0b9d028f9526344a3dab25f2f39bf4f1
jgood <joe.d.good@gmail.com>
2026-02-22 13:03:57 -0600

    putting force_downloads back in since we give it a default

--- sam_audio/model/base.py
+++ sam_audio/model/base.py
@@ -35,10 +35,7 @@ class BaseModel(torch.nn.Module, ModelHubMixin):
                 repo_id=model_id,
                 revision=cls.revision,
                 cache_dir=cache_dir,
-<<<<<<< HEAD
-=======
-                resume_download=resume_download,
->>>>>>> 9f75c207e78e18b7b8a64d96102b3b642c8ab7f9
+                force_download=force_download,
                 token=token,
                 local_files_only=local_files_only,
             )
